9d058398bd63221a79d3f569a1dfb7a7f89b9eda
phucngdev <98637507+phucngdev@users.noreply.github.com>
2024-01-18 22:39:59 +0700

    ko bt làm gì

--- client/src/pages/private/pageAdmin/home.jsx
+++ client/src/pages/private/pageAdmin/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <Header></Header>
         <div className="flex justify-between items-center p-[30px] gap-3">
           <div className="flex items-center justify-between w-[25%] h-[150px] p-2 bg-[#f24f7c] rounded-md cursor-pointer hover:shadow-lg">
-            <div className="flex flex-col gap-2 ">
+            <div className="flex flex-col gap-2 max-w-[60%]">
               <h3 className="text-white">Lượt truy cập</h3>
               <div className="flex items-center gap-2">
                 <h2 className="font-semibold text-xl text-white">3000</h2>
@@ -22,11 +22,11 @@ const Home = () => {
             <FundTwoTone twoToneColor="#dee2e6" style={{ fontSize: "70px" }} />
           </div>
           <div className="flex items-center justify-between w-[25%] h-[150px] p-2 bg-[#716caf] rounded-md cursor-pointer hover:shadow-lg">
-            <div className="flex flex-col gap-2 ">
+            <div className="flex flex-col gap-2 max-w-[60%]">
               <h3 className="text-white">Doanh thu</h3>
               <div className="flex items-center gap-2">
                 <h2 className="font-semibold text-xl text-white">
-                  100.000.000 đ
+                  100.000.000đ
                 </h2>
                 <UpCircleTwoTone twoToneColor="#52c41a" />
               </div>
@@ -37,8 +37,8 @@ const Home = () => {
             <FundTwoTone twoToneColor="#dee2e6" style={{ fontSize: "70px" }} />
           </div>
           <div className="flex items-center justify-between w-[25%] h-[150px] p-2 bg-[#32b0df] rounded-md cursor-pointer hover:shadow-lg">
-            <div className="flex flex-col gap-2 ">
-              <h3 className="text-white">Lượt truy cập</h3>
+            <div className="flex flex-col gap-2 max-w-[60%]">
+              <h3 className="text-white">Đơn hàng</h3>
               <div className="flex items-center gap-2">
                 <h2 className="font-semibold text-xl text-white">3000</h2>
                 <UpCircleTwoTone twoToneColor="#52c41a" />
@@ -50,7 +50,7 @@ const Home = () => {
             <FundTwoTone twoToneColor="#dee2e6" style={{ fontSize: "70px" }} />
           </div>
           <div className="flex items-center justify-between w-[25%] h-[150px] p-2 bg-[#3bbfc2] rounded-md cursor-pointer hover:shadow-lg">
-            <div className="flex flex-col gap-2 ">
+            <div className="flex flex-col gap-2 max-w-[60%]">
               <h3 className="text-white">Lượt truy cập</h3>
               <div className="flex items-center gap-2">
                 <h2 className="font-semibold text-xl text-white">3000</h2>
